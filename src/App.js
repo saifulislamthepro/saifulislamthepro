867f75992860d76b6b34695ba2b10fc7b810dc61
Saiful Islam <mdsaifulislamsaiftech@gmail.com>
2024-02-26 20:16:43 +0600

    adding  insights

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
+import {speedInsights} from '@vercel/speed-insights/react';
 import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
@@ -9,12 +10,14 @@ import Footer from './components/Footer';
 
 function App() {
   return (  
+     
       <><Header />
+      <speedInsights/>
       <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/about" element={<About/>} />
-      <Route path="/services" element={<Services/>} />
-      <Route path="/contact" element={<Contact/>} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/services" element={<Services/>} />
+        <Route path="/contact" element={<Contact/>} />
 
       {/* Add more routes for additional sections */}
     </Routes>
